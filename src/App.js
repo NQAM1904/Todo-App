55486e6ac91d07029ab79d4cb4867c0f2277d787
Minh Nghiem Quoc Anh 3 (FIS TDC HCM) <minhnqa3@fpt.com.vn>
2021-01-04 00:05:42 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <from id="to-do-from">
+            <input type="text" placeholder="Enter text" />
+            <button type="submit">Add</button>
+          </from>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
+
